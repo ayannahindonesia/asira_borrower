6d6aa8bb7348410132dc576d4e6d91107a5dbdb0
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-17 14:41:04 +0700

    168-API check data unique

--- groups/clientGroup.go
+++ groups/clientGroup.go
@@ -10,7 +10,10 @@ import (
 func ClientGroup(e *echo.Echo) {
 	g := e.Group("/client")
 	middlewares.SetClientJWTmiddlewares(g, "client")
+	g.GET("/check_unique", handlers.CheckData)
+	// e.GET("/ganteng", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "/users/:id")
+	// })
 	g.POST("/register_borrower", handlers.RegisterBorrower)
 	g.POST("/borrower_login", handlers.BorrowerLogin)
-	g.GET("/check_unique", handlers.CheckUniqueData)
 }
--- handlers/client_check.go
+++ handlers/client_check.go
@@ -3,30 +3,49 @@ package handlers
 import (
 	"asira/asira"
 	"asira/models"
-	"log"
 	"net/http"
-	"strconv"
+	"strings"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
-	"github.com/thedevsaddam/govalidator"
 )
 
-func CheckUniqueData(c echo.Context) error {
-	defer c.Request().Body.Close()
-
-	borrower := models.Borrower{}
-
-	payloadRules := govalidator.MapData{
-		"idcard_number": []string{"unique:borrowers,idcard_number"},
-		"email":         []string{"email", "unique:borrowers,email"},
-		"phone":         []string{"unique:borrowers,phone"},
+func CheckData(c echo.Context) error {
+	// defer c.Request().Body.Close()
+	var (
+		borrower  models.Borrower
+		cekEmail  bool
+		cekPhone  bool
+		cekIdcard bool
+		cekTaxid  bool
+	)
+	values := []string{}
+	db := asira.App.DB
+	email := c.QueryParam("email")
+	phone := c.QueryParam("phone")
+	idcard_number := c.QueryParam("idcard_number")
+	taxid_number := c.QueryParam("taxid_number")
+	cekEmail = db.Where("email = ?", email).Find(&borrower).RecordNotFound()
+	cekPhone = db.Where("phone = ?", phone).Find(&borrower).RecordNotFound()
+	cekIdcard = db.Where("idcard_number = ?", idcard_number).Find(&borrower).RecordNotFound()
+	cekTaxid = db.Where("taxid_number = ?", taxid_number).Find(&borrower).RecordNotFound()
+	// values = append(values, "Field : ")
+	if cekEmail != true {
+		values = append(values, "email")
+	}
+	if cekPhone != true {
+		values = append(values, "Phone")
+	}
+	if cekIdcard != true {
+		values = append(values, "Id Card Number")
+	}
+	if cekTaxid != true {
+		values = append(values, "Tax Id Number")
 	}
 
-	validate := validateRequestPayload(c, payloadRules, &borrower)
-	if validate != nil {
-		return returnInvalidResponse(http.StatusUnprocessableEntity, validate, "validation error")
+	if values != nil {
+		result := "Field : " + strings.Join(values, " , ") + " Is Used"
+		return returnInvalidResponse(http.StatusInternalServerError, "", result)
 	}
 
-	return c.JSON(http.StatusOK, newBorrower)
+	return c.JSON(http.StatusOK, "OK")
 }
