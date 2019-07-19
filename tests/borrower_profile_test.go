f0e16eeae63f60b9db87352136934c602aa22b67
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-19 15:33:41 +0700

    Adding Unit Testing For Email Password

--- tests/borrower_profile_test.go
+++ tests/borrower_profile_test.go
@@ -75,3 +75,45 @@ func TestBorrowerPatchProfile(t *testing.T) {
 
 	obj.Value("monthly_income").Equal(500)
 }
+
+func TestBorrowerChangePassword(t *testing.T) {
+	RebuildData()
+
+	api := router.NewRouter()
+
+	server := httptest.NewServer(api)
+
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+clientBasicToken)
+	})
+
+	borrowertoken := getBorrowerLoginToken(e, auth, "1")
+
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+borrowertoken)
+	})
+
+	payload := map[string]interface{}{
+		"password": "pass123",
+	}
+
+	// test valid response
+	obj := auth.POST("/borrower/change_password").WithJSON(payload).
+		Expect().
+		Status(http.StatusCreated).JSON().Object()
+	obj.Keys().Contains("id")
+
+	// test empty payload
+	obj = auth.POST("/borrower/change_password").WithJSON(map[string]interface{}{}).
+		Expect().
+		Status(http.StatusUnprocessableEntity).JSON().Object()
+
+	// test unique by registering same data
+	obj = auth.POST("/borrower/change_password").WithJSON(payload).
+		Expect().
+		Status(http.StatusUnprocessableEntity).JSON().Object()
+}
