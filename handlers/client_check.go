e0547eacd27f925dca8053e1b4307af6c5855c96
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-22 14:33:11 +0700

    adding unit testing email

--- handlers/client_check.go
+++ handlers/client_check.go
@@ -17,31 +17,27 @@ func CheckData(c echo.Context) error {
 	var (
 		borrower models.Borrower
 	)
-	values := []string{}
+	var values []string
 	db := asira.App.DB
-	email := c.QueryParam("email")
-	phone := c.QueryParam("phone")
-	idcard_number := c.QueryParam("idcard_number")
-	taxid_number := c.QueryParam("taxid_number")
-	if db.Where("email = ?", email).Find(&borrower).RecordNotFound() {
+
+	if email := c.QueryParam("email"); email != "" && !db.Where("email = ?", email).Find(&borrower).RecordNotFound() {
 		values = append(values, "Email")
 	}
-	if db.Where("phone = ?", phone).Find(&borrower).RecordNotFound() {
+	if phone := c.QueryParam("phone"); phone != "" && !db.Where("phone = ?", phone).Find(&borrower).RecordNotFound() {
 		values = append(values, "Phone")
 	}
-	if db.Where("idcard_number = ?", idcard_number).Find(&borrower).RecordNotFound() {
+	if idcard_number := c.QueryParam("idcard_number"); idcard_number != "" && !db.Where("idcard_number = ?", idcard_number).Find(&borrower).RecordNotFound() {
 		values = append(values, "Id Card Number")
 	}
-	if db.Where("taxid_number = ?", taxid_number).Find(&borrower).RecordNotFound() {
+	if taxid_number := c.QueryParam("taxid_number"); taxid_number != "" && !db.Where("taxid_number = ?", taxid_number).Find(&borrower).RecordNotFound() {
 		values = append(values, "Tax Id Number")
 	}
-
-	if values != nil {
-		result := "Field : " + strings.Join(values, " , ") + " Is Used"
-		return returnInvalidResponse(http.StatusInternalServerError, "", result)
+	if len(values) < 1 {
+		return c.JSON(http.StatusOK, "OK")
 	}
+	result := "Field : " + strings.Join(values, " , ") + " Is Used"
+	return returnInvalidResponse(http.StatusUnprocessableEntity, "", result)
 
-	return c.JSON(http.StatusOK, "OK")
 }
 
 func CheckEmail(c echo.Context) error {
