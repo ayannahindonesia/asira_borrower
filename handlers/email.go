3b9c9eca1eaa4b3d6b0d3b58455095708a539ce6
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-23 15:00:18 +0700

    Set Config Email

--- handlers/email.go
+++ handlers/email.go
@@ -1,26 +1,23 @@
 package handlers
 
 import (
+	"asira/asira"
 	"fmt"
 	"net/smtp"
 	"strings"
 )
 
-const CONFIG_SMTP_HOST = "smtp.gmail.com"
-const CONFIG_SMTP_PORT = 587
-const CONFIG_EMAIL = "apranta123@gmail.com"
-const CONFIG_PASSWORD = "Animajid01"
-
 func sendMail(to []string, subject, message string) error {
-	body := "From: " + CONFIG_EMAIL + "\n" +
+	Config := asira.App.Config.GetStringMap(fmt.Sprintf("%s.mailer", asira.App.ENV))
+	body := "From: " + Config["SMTP_HOST"].(string) + "\n" +
 		"To: " + strings.Join(to, ",") + "\n" +
 		"Subject: " + subject + "\n\n" +
 		message
 
-	auth := smtp.PlainAuth("", CONFIG_EMAIL, CONFIG_PASSWORD, CONFIG_SMTP_HOST)
-	smtpAddr := fmt.Sprintf("%s:%d", CONFIG_SMTP_HOST, CONFIG_SMTP_PORT)
+	auth := smtp.PlainAuth("", Config["EMAIL"].(string), Config["PASSWORD"].(string), Config["SMTP_HOST"].(string))
+	smtpAddr := fmt.Sprintf("%s:%d", Config["SMTP_HOST"].(string), Config["SMTP_PORT"].(int))
 
-	err := smtp.SendMail(smtpAddr, auth, CONFIG_EMAIL, append(to), []byte(body))
+	err := smtp.SendMail(smtpAddr, auth, Config["EMAIL"].(string), append(to), []byte(body))
 	if err != nil {
 		return err
 	}
