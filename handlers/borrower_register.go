e9363deef7f868b21d7901e9db56d0229938f33b
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-29 16:26:01 +0700

    create image store

--- handlers/borrower_register.go
+++ handlers/borrower_register.go
@@ -28,70 +28,68 @@ type (
 
 func RegisterBorrower(c echo.Context) error {
 	defer c.Request().Body.Close()
-	var image = Image{Image_string: result["idcard_image"]}
 	var result map[string]interface{}
 	json.NewDecoder(c.Request().Body).Decode(&result)
-	asira.App.DB.Create(&image)
-	borrower := models.Borrower{}
+	// borrower := models.Borrower{}
 
-	payloadRules := govalidator.MapData{
-		"fullname":              []string{"required"},
-		"gender":                []string{"required"},
-		"idcard_number":         []string{"required", "unique:borrowers,idcard_number"},
-		"taxid_number":          []string{"unique:borrowers,taxid_number"},
-		"email":                 []string{"email", "unique:borrowers,email"},
-		"birthday":              []string{"date"},
-		"birthplace":            []string{"required"},
-		"last_education":        []string{"required"},
-		"mother_name":           []string{"required"},
-		"phone":                 []string{"required"},
-		"marriage_status":       []string{"required"},
-		"spouse_name":           []string{},
-		"spouse_birthday":       []string{"date"},
-		"spouse_lasteducation":  []string{},
-		"dependants":            []string{},
-		"address":               []string{"required"},
-		"province":              []string{"required"},
-		"city":                  []string{"required"},
-		"neighbour_association": []string{"required"},
-		"hamlets":               []string{"required"},
-		"home_phonenumber":      []string{},
-		"subdistrict":           []string{"required"},
-		"urban_village":         []string{"required"},
-		"home_ownership":        []string{"required"},
-		"lived_for":             []string{"required"},
-		"occupation":            []string{"required"},
-		"employee_id":           []string{},
-		"employer_name":         []string{"required"},
-		"employer_address":      []string{"required"},
-		"department":            []string{"required"},
-		"been_workingfor":       []string{"required"},
-		"direct_superiorname":   []string{},
-		"employer_number":       []string{"required"},
-		"monthly_income":        []string{"required"},
-		"other_income":          []string{},
-		"other_incomesource":    []string{},
-		"field_of_work":         []string{"required"},
-		"related_personname":    []string{"required"},
-		"related_relation":      []string{"required"},
-		"related_phonenumber":   []string{"required"},
-		"related_homenumber":    []string{},
-		"bank":                  []string{},
-		"bank_accountnumber":    []string{"unique:borrowers,bank_accountnumber"},
-		"password":              []string{"required"},
-	}
+	// payloadRules := govalidator.MapData{
+	// 	"fullname":              []string{"required"},
+	// 	"gender":                []string{"required"},
+	// 	"idcard_number":         []string{"required", "unique:borrowers,idcard_number"},
+	// 	"taxid_number":          []string{"unique:borrowers,taxid_number"},
+	// 	"email":                 []string{"email", "unique:borrowers,email"},
+	// 	"birthday":              []string{"date"},
+	// 	"birthplace":            []string{"required"},
+	// 	"last_education":        []string{"required"},
+	// 	"mother_name":           []string{"required"},
+	// 	"phone":                 []string{"required"},
+	// 	"marriage_status":       []string{"required"},
+	// 	"spouse_name":           []string{},
+	// 	"spouse_birthday":       []string{"date"},
+	// 	"spouse_lasteducation":  []string{},
+	// 	"dependants":            []string{},
+	// 	"address":               []string{"required"},
+	// 	"province":              []string{"required"},
+	// 	"city":                  []string{"required"},
+	// 	"neighbour_association": []string{"required"},
+	// 	"hamlets":               []string{"required"},
+	// 	"home_phonenumber":      []string{},
+	// 	"subdistrict":           []string{"required"},
+	// 	"urban_village":         []string{"required"},
+	// 	"home_ownership":        []string{"required"},
+	// 	"lived_for":             []string{"required"},
+	// 	"occupation":            []string{"required"},
+	// 	"employee_id":           []string{},
+	// 	"employer_name":         []string{"required"},
+	// 	"employer_address":      []string{"required"},
+	// 	"department":            []string{"required"},
+	// 	"been_workingfor":       []string{"required"},
+	// 	"direct_superiorname":   []string{},
+	// 	"employer_number":       []string{"required"},
+	// 	"monthly_income":        []string{"required"},
+	// 	"other_income":          []string{},
+	// 	"other_incomesource":    []string{},
+	// 	"field_of_work":         []string{"required"},
+	// 	"related_personname":    []string{"required"},
+	// 	"related_relation":      []string{"required"},
+	// 	"related_phonenumber":   []string{"required"},
+	// 	"related_homenumber":    []string{},
+	// 	"bank":                  []string{},
+	// 	"bank_accountnumber":    []string{"unique:borrowers,bank_accountnumber"},
+	// 	"password":              []string{"required"},
+	// }
 
-	validate := validateRequestPayload(c, payloadRules, &borrower)
-	if validate != nil {
-		return returnInvalidResponse(http.StatusUnprocessableEntity, validate, "validation error")
-	}
+	// validate := validateRequestPayload(c, payloadRules, &borrower)
+	// if validate != nil {
+	// 	return returnInvalidResponse(http.StatusUnprocessableEntity, validate, "validation error")
+	// }
 
-	newBorrower, err := borrower.Create()
-	if err != nil {
-		return returnInvalidResponse(http.StatusInternalServerError, err, "create new borrower failed")
-	}
+	// newBorrower, err := borrower.Create()
+	// if err != nil {
+	// 	return returnInvalidResponse(http.StatusInternalServerError, err, "create new borrower failed")
+	// }
 
-	return c.JSON(http.StatusCreated, newBorrower)
+	return c.JSON(http.StatusCreated, Id)
 }
 
 func RequestOTPverifyAccount(c echo.Context) error {
