add9ad59643eea38a57a8940fa588481b00211ed
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-07-30 10:58:04 +0700

    merge maaster

--- handlers/borrower_register.go
+++ handlers/borrower_register.go
@@ -81,7 +81,7 @@ func RegisterBorrower(c echo.Context) error {
 
 	newBorrower, err := borrower.Create()
 	if err != nil {
-		return returnInvalidResponse(http.StatusInternalServerError, err, "create new borrower failed")
+		return returnInvalidResponse(http.StatusInternalServerError, err, "Pembuatan Akun Gagal")
 	}
 
 	return c.JSON(http.StatusCreated, newBorrower)
@@ -111,7 +111,7 @@ func RequestOTPverifyAccount(c echo.Context) error {
 	otpCode := asira.App.OTP.HOTP.At(int(counter))
 	log.Printf("generate otp code for phonenumber %s : %s", otpRequest.Phone, otpCode)
 
-	return c.JSON(http.StatusOK, map[string]interface{}{"message": "OTP Sent"})
+	return c.JSON(http.StatusOK, map[string]interface{}{"message": "OTP Terkirim"})
 }
 
 func VerifyAccountOTP(c echo.Context) error {
