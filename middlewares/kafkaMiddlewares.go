e4086da1ae6766b1c7f6d9ed183ffced163c1af0
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-08-16 17:58:33 +0700

    general for middleware

---
+++ middlewares/kafkaMiddlewares.go
@@ -0,0 +1,116 @@
+package middlewares
+
+import (
+	"asira_borrower/asira"
+	"asira_borrower/models"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+type (
+	AsiraKafkaHandlers struct {
+		KafkaConsumer     sarama.Consumer
+		PartitionConsumer sarama.PartitionConsumer
+	}
+
+	BanksData struct {
+		Data interface{} `json:"banks"`
+	}
+
+	LoanStatus struct {
+		Loan interface{} `json:"loan"`
+	}
+)
+
+func init() {
+	topics := asira.App.Config.GetStringMap(fmt.Sprintf("%s.kafka.topics", asira.App.ENV))
+
+	kafka := &AsiraKafkaHandlers{}
+	kafka.KafkaConsumer = asira.App.Kafka.Consumer
+
+	kafka.SetPartitionConsumer(topics["new_bank"].(string))
+
+	go func() {
+		for {
+			message, err := kafka.Listen()
+			if err != nil {
+				log.Printf("error occured when listening kafka : %v", err)
+			}
+			if message != nil {
+				err = syncBankData(message)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}()
+}
+
+func (k *AsiraKafkaHandlers) SetPartitionConsumer(topic string) (err error) {
+	k.PartitionConsumer, err = k.KafkaConsumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (k *AsiraKafkaHandlers) Listen() ([]byte, error) {
+	select {
+	case err := <-k.PartitionConsumer.Errors():
+		return nil, err
+	case msg := <-k.PartitionConsumer.Messages():
+		return msg.Value, nil
+	}
+
+	return nil, fmt.Errorf("unidentified error while listening")
+}
+
+func syncBankData(kafkaMessage []byte) (err error) {
+	var banksData BanksData
+	var bank models.Bank
+	err = json.Unmarshal(kafkaMessage, &banksData)
+	if err != nil {
+		return err
+	}
+
+	marshal, err := json.Marshal(banksData.Info)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(marshal, &bank)
+	if err != nil {
+		return err
+	}
+
+	_, err = bank.Save()
+	return err
+
+}
+
+func loanUpdate(kafkaMessage [byte] (err error)){
+	var loanData LoanStatus
+	var loan models.Loan
+
+	err = json.Unmarshal(kafkaMessage , loanData)
+	if err != nil {
+		return err
+	}
+
+	marshal, err := json.Marshal(loanData.Loan)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(marshal, &loan)
+	if err != nil {
+		return err
+	}
+
+	_, err = loan.Save()
+	return err
+}
