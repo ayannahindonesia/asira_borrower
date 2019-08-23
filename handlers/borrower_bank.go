f01e3caa1690636fe0ad5385420a36c221ab3519
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-08-23 11:21:06 +0700

    banks

--- handlers/borrower_bank.go
+++ handlers/borrower_bank.go
@@ -1,25 +1,46 @@
 package handlers
 
 import (
+	"asira_borrower/models"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 )
 
 func BorrowerBankService(c echo.Context) error {
-	defer c.Request().Body.Close()
+	user := c.Get("user")
+	token := user.(*jwt.Token)
+	claims := token.Claims.(jwt.MapClaims)
 
-	bank_service_id, _ := strconv.Atoi(c.Param("bank_service_id"))
+	borrowerModel := models.Borrower{}
 
-	bankService := models.BankService{}
-	result, err := bankService.FindbyID(bank_service_id)
+	borrowerID, _ := strconv.Atoi(claims["jti"].(string))
+	borrower, err := borrowerModel.FindbyID(borrowerID)
 	if err != nil {
-		return returnInvalidResponse(http.StatusNotFound, err, fmt.Sprintf("layanan %v tidak ditemukan", bank_service_id))
+		return returnInvalidResponse(http.StatusForbidden, err, "Akun tidak ditemukan")
 	}
 
-	return c.JSON(http.StatusOK, result)
+	bank := models.Bank{}
+	bankBorrower, _ := bank.FindbyID(int(borrower.Bank.Int64))
+
+	var service []int
+
+	if err := json.Unmarshal(bankBorrower.Products, &service); err != nil {
+		log.Fatal(err)
+	}
+
+	bankService := models.BankService{}
+	bServices := make([]interface{}, len(service))
+	for i, s := range service {
+		bServices[i], err = bankService.FindbyID(service[i])
+	}
+
+	return c.JSON(http.StatusOK, bServices)
 }
 
 func BorrowerBankProduct(c echo.Context) error {
