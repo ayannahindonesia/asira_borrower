6815ee70b642c00b982236a50209aeb919eddc35
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-08-23 17:07:49 +0700

    fix problem

--- migration/00001_init_tables.sql
+++ migration/00001_init_tables.sql
@@ -1,6 +1,14 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
+CREATE TABLE "images" (
+    "id" bigserial,
+    "image_string" text,
+    "created_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
+    "updated_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("id")
+) WITH (OIDS = FALSE);
+
 CREATE TABLE "bank_types" (
     "id" bigserial,
     "created_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
@@ -60,14 +68,6 @@ CREATE TABLE "service_products" (
     "assurance" varchar(255),
     "status" varchar(255),
     FOREIGN KEY ("service") REFERENCES bank_services(id),
-    PRIMARY KEY ("id")
-) WITH (OIDS = FALSE);
-
-CREATE TABLE "images" (
-    "id" bigserial,
-    "image_string" text,
-    "created_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
-    "updated_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 ) WITH (OIDS = FALSE);
 
