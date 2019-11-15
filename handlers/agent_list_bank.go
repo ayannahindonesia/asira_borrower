b8409e4a45a83c2b9fb269d65b8aa0828f6d9d4c
roy adventus <radventus@ayannah.com>
2019-11-15 21:16:51 +0700

    endpoint : /agent/banks
    get list of banks that owned by current agent (jti agent)

--- handlers/agent_list_bank.go
+++ handlers/agent_list_bank.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
+	"gitlab.com/asira-ayannah/basemodel"
 
 	"github.com/labstack/echo"
 )
@@ -17,10 +18,19 @@ type BanksOfAgentSelect struct {
 	BankName string `json:"bank_name"`
 }
 
-func AgentProfile(c echo.Context) error {
-	defer c.Request().Body.Close()
-	var agentBanks []BanksOfAgentSelect
+type BankList struct {
+	ID       int64  `json:"id"`
+	BankName string `json:"bank_name"`
+}
 
+func AgentAllBank(c echo.Context) error {
+	defer c.Request().Body.Close()
+	var (
+		agentBanks []BanksOfAgentSelect
+		banklist   []BankList
+		totalRows  int
+		i          int
+	)
 	user := c.Get("user")
 	token := user.(*jwt.Token)
 	claims := token.Claims.(jwt.MapClaims)
@@ -41,5 +51,23 @@ func AgentProfile(c echo.Context) error {
 		return returnInvalidResponse(http.StatusForbidden, err, "Akun tidak ditemukan")
 	}
 
-	return c.JSON(http.StatusOK, agentBanks)
+	//get IDs of bank dari array pertama biar lebih aman
+	bankIDs := agentBanks[0].Banks
+
+	for _, val := range agentBanks {
+		newBankList := BankList{
+			ID:       bankIDs[i],
+			BankName: val.BankName,
+		}
+		banklist = append(banklist, newBankList)
+		i++
+	}
+	//create custom response
+	tempDB := db
+	tempDB.Count(&totalRows)
+	result := basemodel.PagedFindResult{
+		TotalData: totalRows,
+		Data:      banklist,
+	}
+	return c.JSON(http.StatusOK, result)
 }
