fe4eb0f312289fe379067d6f1baf0a934b147c27
roy adventus <radventus@ayannah.com>
2019-11-18 19:26:28 +0700

    revised : inner join to left join

--- handlers/agent_list_bank.go
+++ handlers/agent_list_bank.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"asira_borrower/asira"
-	"asira_borrower/models"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -13,55 +12,37 @@ import (
 	"github.com/labstack/echo"
 )
 
-type BanksOfAgentSelect struct {
-	models.Agent
-	BankName string `json:"bank_name"`
-}
-
 type BankList struct {
-	ID       int64  `json:"id"`
-	BankName string `json:"bank_name"`
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
 }
 
 func AgentAllBank(c echo.Context) error {
 	defer c.Request().Body.Close()
 	var (
-		agentBanks []BanksOfAgentSelect
-		banklist   []BankList
-		totalRows  int
-		i          int
+		banklist  []BankList
+		totalRows int
 	)
 	user := c.Get("user")
 	token := user.(*jwt.Token)
 	claims := token.Claims.(jwt.MapClaims)
-	agentID, _ := strconv.Atoi(claims["jti"].(string))
+	agentID, err := strconv.Atoi(claims["jti"].(string))
 
-	//custom query
-	db := asira.App.DB
-	db = db.Table("agents a").
-		Select("a.*, b.name as bank_name").
-		Joins("INNER JOIN banks b ON b.id = ANY(a.banks)").
-		Where("a.id = ?", agentID)
-	//query
-	err = db.Find(&agentBanks).Error
-	if err != nil {
-		fmt.Println(err)
-	}
 	if err != nil {
 		return returnInvalidResponse(http.StatusForbidden, err, "Akun tidak ditemukan")
 	}
-
-	//get IDs of bank dari array pertama biar lebih aman
-	bankIDs := agentBanks[0].Banks
-
-	for _, val := range agentBanks {
-		newBankList := BankList{
-			ID:       bankIDs[i],
-			BankName: val.BankName,
-		}
-		banklist = append(banklist, newBankList)
-		i++
+	//custom query
+	db := asira.App.DB
+	db = db.Table("banks b").
+		Select("b.id, b.name").
+		Joins("LEFT JOIN agents a ON b.id = ANY(a.banks)").
+		Where("a.id = ?", agentID)
+	//query
+	err = db.Find(&banklist).Error
+	if err != nil {
+		fmt.Println(err)
 	}
+
 	//create custom response
 	tempDB := db
 	tempDB.Count(&totalRows)
