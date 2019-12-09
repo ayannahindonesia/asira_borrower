f6b58128d952b856f102a81d47362f130c8edf87
royadv <radventus@ayannah.com>
2019-12-09 15:13:00 +0700

    init agent's patch

--- handlers/agent_profile.go
+++ handlers/agent_profile.go
@@ -6,10 +6,12 @@ import (
 	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/thedevsaddam/govalidator"
 
 	"github.com/labstack/echo"
 )
 
+//AgentProfile get current agent's profile
 func AgentProfile(c echo.Context) error {
 	defer c.Request().Body.Close()
 
@@ -25,5 +27,36 @@ func AgentProfile(c echo.Context) error {
 		return returnInvalidResponse(http.StatusForbidden, err, "Akun tidak ditemukan")
 	}
 
+	return c.JSON(http.StatusOK, agentModel)
+}
+
+//AgentProfileEdit update current agent's profile
+func AgentProfileEdit(c echo.Context) error {
+	defer c.Request().Body.Close()
+
+	user := c.Get("user")
+	token := user.(*jwt.Token)
+	claims := token.Claims.(jwt.MapClaims)
+
+	agentModel := models.Agent{}
+
+	agentID, _ := strconv.Atoi(claims["jti"].(string))
+	err := agentModel.FindbyID(agentID)
+	if err != nil {
+		return returnInvalidResponse(http.StatusForbidden, err, "Akun tidak ditemukan")
+	}
+
+	validate := validateRequestPayload(c, payloadRules, &borrowerModel)
+	if validate != nil {
+		return returnInvalidResponse(http.StatusUnprocessableEntity, validate, "validation error")
+	}
+
+	agentModel.Password := password
+
+	payloadRules := govalidator.MapData{
+		"name":  []string{},
+		"email": []string{"unique_edit:agents,email"},
+		"phone": []string{"phone", "unique:agents,taxid_number"},
+	}
 	return c.JSON(http.StatusOK, agentModel)
 }
---
+++ tests/agent_profile_test.go
@@ -0,0 +1,93 @@
+package tests
+
+import (
+	"asira_borrower/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gavv/httpexpect"
+)
+
+func TestAgentGetProfile(t *testing.T) {
+	RebuildData()
+
+	api := router.NewRouter()
+
+	server := httptest.NewServer(api)
+
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+clientBasicToken)
+	})
+
+	agentToken := getAgentLoginToken(e, auth, "1")
+
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+agentToken)
+	})
+
+	// valid response
+	obj := auth.GET("/agent/profile").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+	obj.ContainsKey("username").ValueEqual("username", "agentJ")
+
+	// wrong token
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer wrongtoken")
+	})
+	auth.GET("/agent/profile").WithJSON(map[string]interface{}{}).
+		Expect().
+		Status(http.StatusUnauthorized).JSON().Object()
+}
+
+func TestAgentPatchProfile(t *testing.T) {
+	RebuildData()
+
+	api := router.NewRouter()
+
+	server := httptest.NewServer(api)
+
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+clientBasicToken)
+	})
+
+	agentToken := getAgentLoginToken(e, auth, "1")
+
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+agentToken)
+	})
+
+	// valid response
+	data := map[string]interface{}{
+		"name": "agent Z",
+		"phone": "081234567899",
+		"email": "agentZ@mail.com"
+	}
+	obj := auth.PATCH("/agent/profile").WithJSON(data).
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+
+	obj.Value("monthly_income").Equal(500)
+
+	obj := auth.GET("/agent/profile").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+	obj.ContainsKey("username").ValueEqual("username", "agentJ")
+
+	// wrong token
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer wrongtoken")
+	})
+	auth.GET("/agent/profile").WithJSON(map[string]interface{}{}).
+		Expect().
+		Status(http.StatusUnauthorized).JSON().Object()
+}
