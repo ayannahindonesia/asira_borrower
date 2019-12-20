b1e38a94a4dea5240f891c88ed170eb4475ba1c6
royadv <radventus@ayannah.com>
2019-12-20 16:36:16 +0700

    rollback admin test

--- tests/admin_borrower_test.go
+++ tests/admin_borrower_test.go
@@ -1,69 +1,74 @@
 package tests
 
 import (
+	"asira_borrower/router"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gavv/httpexpect"
 )
 
 func TestBorrowerGetAll(t *testing.T) {
-	// 	RebuildData()
+	RebuildData()
 
-	// 	api := router.NewRouter()
+	api := router.NewRouter()
 
-	// 	server := httptest.NewServer(api)
+	server := httptest.NewServer(api)
 
-	// 	defer server.Close()
+	defer server.Close()
 
-	// 	e := httpexpect.New(t, server.URL)
+	e := httpexpect.New(t, server.URL)
 
-	// 	auth := e.Builder(func(req *httpexpect.Request) {
-	// 		req.WithHeader("Authorization", "Basic "+adminBasicToken)
-	// 	})
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+adminBasicToken)
+	})
 
-	// 	obj := auth.GET("/clientauth").
-	// 		Expect().
-	// 		Status(http.StatusOK).JSON().Object()
+	obj := auth.GET("/clientauth").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
 
-	// 	admintoken := obj.Value("token").String().Raw()
+	admintoken := obj.Value("token").String().Raw()
 
-	// 	auth = e.Builder(func(req *httpexpect.Request) {
-	// 		req.WithHeader("Authorization", "Bearer "+admintoken)
-	// 	})
-	// 	// valid response of borrowers
-	// 	obj = auth.GET("/admin/borrower").
-	// 		Expect().
-	// 		Status(http.StatusOK).JSON().Object()
-	// }
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+admintoken)
+	})
+	// valid response of borrowers
+	obj = auth.GET("/admin/borrower").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+}
 
-	// func TestBorrowerGetDetails(t *testing.T) {
-	// 	RebuildData()
+func TestBorrowerGetDetails(t *testing.T) {
+	RebuildData()
 
-	// 	api := router.NewRouter()
+	api := router.NewRouter()
 
-	// 	server := httptest.NewServer(api)
+	server := httptest.NewServer(api)
 
-	// 	defer server.Close()
+	defer server.Close()
 
-	// 	e := httpexpect.New(t, server.URL)
+	e := httpexpect.New(t, server.URL)
 
-	// 	auth := e.Builder(func(req *httpexpect.Request) {
-	// 		req.WithHeader("Authorization", "Basic "+adminBasicToken)
-	// 	})
-	// 	obj := auth.GET("/clientauth").
-	// 		Expect().
-	// 		Status(http.StatusOK).JSON().Object()
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+adminBasicToken)
+	})
+	obj := auth.GET("/clientauth").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
 
-	// 	admintoken := obj.Value("token").String().Raw()
+	admintoken := obj.Value("token").String().Raw()
 
-	// 	auth = e.Builder(func(req *httpexpect.Request) {
-	// 		req.WithHeader("Authorization", "Bearer "+admintoken)
-	// 	})
-	// 	// valid response of loan details
-	// 	obj = auth.GET("/admin/borrower/1").
-	// 		Expect().
-	// 		Status(http.StatusOK).JSON().Object()
-	// 	obj.ContainsKey("id").ValueEqual("id", 1)
-	// 	// loan id not found
-	// 	obj = auth.GET("/admin/borrower/99").
-	// 		Expect().
-	// 		Status(http.StatusNotFound).JSON().Object()
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+admintoken)
+	})
+	// valid response of loan details
+	obj = auth.GET("/admin/borrower/1").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+	obj.ContainsKey("id").ValueEqual("id", 1)
+	// loan id not found
+	obj = auth.GET("/admin/borrower/99").
+		Expect().
+		Status(http.StatusNotFound).JSON().Object()
 }
